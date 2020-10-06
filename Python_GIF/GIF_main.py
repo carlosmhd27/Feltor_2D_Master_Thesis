b9923c5e31f4291e3ecd3c9d393db9ad0d7b4f42
carlosmhd27 <64638438+carlosmhd27@users.noreply.github.com>
2020-10-06 18:34:47 +0200

    Update GIF_main.py

--- Python_GIF/GIF_main.py
+++ Python_GIF/GIF_main.py
@@ -3,53 +3,88 @@ import matplotlib
 matplotlib.use("Agg")
 
 from GIF_modules import Analyzed, animate, init
+from datetime    import datetime
 
 import numpy                as np
 import matplotlib.animation as animation
 import matplotlib.pyplot    as plt
+import time
 
 ## General variables
-fps = 10
-model = 'IC'
-
-## Initiation of the figure
-fig, ax = plt.subplots(2, 3, figsize=(24, 12))
+fps = 5
+i   = 1   ## to reduce the number of points we use
 
 ## We define the directory where the outputs are located and confirm it exists
-dir_name  = 'path/to/outputs/'
-directory = './' + dir_name
+dir_name  = '../marconi_outputs/hdiff_outputs/Per_Per/'
 exists    = os.path.isdir("./" + dir_name)
 
+## We define the file of the output and confirm it exists
 if not exists:
     raise Exception('The directory does not exist.')
     
-## We define the file of the output and confirm it exists
-# for model in ['IC', 'HW_ord', 'HW_mod']:
-File_name = os.path.join(dir_name, 'output_{}.nc'.format(model))
-existance = os.path.exists(File_name)
+models = ['IC', 'HW_mod', 'HW_ord'] ## , 'IC', 'HW_mod', 'HW_ord' 'HW_ord_dt_0.01', 'HW_ord_dt_0.001', 'HW_ord_dt_0.0001', 'HW_ord_dt_1e-05'
+extra  = '_Neumann'
 
-if not existance:
-    raise Exception('The File does not exist.')
+for model in models:
+    
+    GIF_name = dir_name + f'video_{model + extra}.gif'
+    if os.path.exists(GIF_name):
+        overwrite = input("The GIF file already exists, do you wanna overwrite it?([Y]/n)\n")
+        if overwrite not in ['y', 'ye', 'yes', 'Y', 'YE', 'YES', None]:
+            GIF_name = input('Please, give a new name')
+            if GIF_name == None:
+                raise Exception('You must change the name of the GIF file')
+            
 
-print('+' * 50)
-print(r'model: {}'.format(model))
-print('+' * 50)
+    info_file = dir_name + f"info_{model + extra}.txt"
+    if os.path.exists(info_file):
+        os.remove(info_file)
 
-## We analysis the file and obtain the values we wanna measure
-Analytics = Analyzed(File_name)
+    start = time.time()
+    File_name = os.path.join(dir_name, f'output_{model}.nc')
+    existance = os.path.exists(File_name)
 
-## Generate the init function for the GIF, it needs the model the fig and the parameters
-init_ = lambda : init(model = model, Analytics = Analytics, ax = ax, fig = fig)
+    ## Initiation of the figure
+    fig, ax = plt.subplots(2, 3, figsize=(24, 12))
 
-## We define the format for writing the mp4 file
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=fps, metadata=dict(artist='Me'), bitrate=600)
+    information = open(info_file, 'a')
 
-## We define the animation class and we save the animation, or show it 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(Analytics.ions), 3), fargs = (Analytics, ax),
-                              init_func=init_, interval = 100)
+    if not existance:
+        information.write(f'{File_name} does not exist.')
 
-ani.save('video_{}.mp4'.format(model), writer = writer)
-# plt.show()
+    else:
 
+        ## We analysis the file and obtain the values we wanna measure
+        Analytics = Analyzed(File_name)
+
+
+        information.write('+' * 50 + "\n")
+        information.write(f'model: {model + extra}, with {len(Analytics.ions) // i} out of {len(Analytics.ions)} points in the GIF' + "\n")
+        information.write(f'The times goes from {Analytics.time[0]} to {Analytics.time[-1]}' + "\n")
+        information.write('+' * 50 + "\n")
+        information.write(f'The file started at {datetime.now()}'+ '\n')
+        information.write('+' * 50 + "\n")
+        information.close()
+
+        ## Generate the init function for the GIF, it needs the model the fig and the parameters
+        init_ = lambda : init(model = model + extra, Analytics = Analytics, ax = ax, fig = fig)
+
+        ## We define the format for writing the mp4 file
+#         Writer = animation.writers['ffmpeg']
+        writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate = 600)
+
+        ## We define the animation class and we save the animation, or show it 
+        ani = animation.FuncAnimation(fig, animate, np.arange(0, len(Analytics.ions), i),
+                                      fargs = (Analytics, ax),
+                                      init_func=init_, interval = 100)
+
+        ani.save(GIF_name)
+        # plt.show()
+        plt.clf()
+
+        stop = time.time()
+        information = open(info_file, 'a')
+        information.write('After {:.1f}h, I am done with model: {}'.format((stop - start) / 3600, model + extra) + 2 * "\n")
+    information.close()
+    
 print('Done')
