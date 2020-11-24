00470f47006edd12e8181848c727376d843a6e0e
Carlos Rodríguez <64638438+carlosmhd27@users.noreply.github.com>
2020-11-24 15:29:11 +0100

    Update GIF_main.py
    Make it look clean and sharp

--- Python_GIF/GIF_main.py
+++ Python_GIF/GIF_main.py
@@ -1,14 +1,17 @@
-import os
-import matplotlib
-matplotlib.use("Agg")
-
 from GIF_modules import Analyzed, animate, init
 from datetime    import datetime
+from os.path     import join, exists, isdir
+from os          import remove
+from sys         import argv
+from time        import time
+from numpy       import arange
+from mpi4py      import MPI
 
-import numpy                as np
-import matplotlib.animation as animation
-import matplotlib.pyplot    as plt
-import time
+from matplotlib.pyplot    import clf, show, subplots
+from matplotlib.animation import writers, FuncAnimation, PillowWriter
+from matplotlib           import use
+
+use("Agg")
 
 ## General variables
 fps = 5
@@ -17,15 +20,15 @@ i   = 1   ## to reduce the number of points we use
 ## We define the directory where the outputs are located and confirm it exists
 dir_name  = '../marconi_outputs/hdiff_outputs/Per_Per/'
 
-assert os.path.isdir("./" + dir_name), 'The directory does not exist.'
+assert isdir("./" + dir_name), 'The directory does not exist.'
     
 models = ['IC', 'HW_mod', 'HW_ord'] ## , 'IC', 'HW_mod', 'HW_ord' 'HW_ord_dt_0.01', 'HW_ord_dt_0.001', 'HW_ord_dt_0.0001', 'HW_ord_dt_1e-05'
 extra  = '_Neumann'
 
 for model in models:
     
-    GIF_name = dir_name + f'video_{model + extra}.gif'
-    if os.path.exists(GIF_name):
+    GIF_name = join(dir_name, f'video_{model + extra}.gif')
+    if exists(GIF_name):
         overwrite = input("The GIF file already exists, do you wanna overwrite it?([Y]/n)\n")
         if overwrite not in ['y', 'ye', 'yes', 'Y', 'YE', 'YES', '']:
             GIF_name = input('Please, give a new name')
@@ -33,16 +36,16 @@ for model in models:
                 continue
             
 
-    info_file = dir_name + f"info_{model + extra}.txt"
-    if os.path.exists(info_file):
-        os.remove(info_file)
+    info_file = join(dir_name, f"info_{model + extra}.txt")
+    if exists(info_file):
+        remove(info_file)
 
-    start = time.time()
-    File_name = os.path.join(dir_name, f'output_{model}.nc')
-    existance = os.path.exists(File_name)
+    start     = time()
+    File_name = join(dir_name, f'output_{model}.nc')
+    existance = exists(File_name)
 
     ## Initiation of the figure
-    fig, ax = plt.subplots(2, 3, figsize=(24, 12))
+    fig, ax = subplots(2, 3, figsize=(24, 12))
 
     if not existance:
         with open(info_file, 'a') as information:
@@ -66,22 +69,22 @@ for model in models:
     animate_ = lambda pos, An, a: animate(pos, An, a, fig = fig)
 
     ## We define the format for writing the mp4 file
-#         Writer = animation.writers['ffmpeg']
-    writer = animation.PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate = 600)
+#         Writer = writers['ffmpeg']
+    writer = PillowWriter(fps=fps, metadata=dict(artist='Me'), bitrate = 600)
 
     ## We define the animation class and we save the animation, or show it 
-    ani = animation.FuncAnimation(fig, animate_, np.arange(0, len(Analytics.ions), i),
+    ani = FuncAnimation(fig, animate_, np.arange(0, len(Analytics.ions), i),
                                   fargs = (Analytics, ax),
                                   init_func=init_, interval = 100) ## np.arange(1, len(Analytics.ions))
 
     ani.save(GIF_name)
-    # plt.show()
-    plt.clf()
+    # show()
+    clf()
 
-    stop = time.time()
+    stop   = time()
     needed = stop - start
-    nd_hr = needed // 3600
-    nd_mn = (needed % 3600) * 60
+    nd_hr  = needed // 3600
+    nd_mn  = (needed % 3600) * 60
     with open(info_file, 'a') as information:
         information.write('After {} h and {:.1f} min, I am done with model: {}'.format(nd_mn, nd_hr, model + extra) + 2 * "\n")
     
