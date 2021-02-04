4b07a9da3f797d6addb24137ae4ace6d583a0836
Carlos Rodríguez <64638438+carlosmhd27@users.noreply.github.com>
2021-02-04 09:43:58 +0100

    Update GIF_modules.py

--- Python_GIF/GIF_modules.py
+++ Python_GIF/GIF_modules.py
@@ -1,25 +1,19 @@
-import matplotlib
-matplotlib.use("Agg")
-
-import sys
-sys.path.insert(1, '../2D_FELTOR_Analysis/')
+from sys import path
+path.insert(1, '../2D_FELTOR_Analysis/')
 
 from Analysis          import Analyse
-from matplotlib.pyplot import axis, savefig, pcolormesh, imshow, show, figure
-from matplotlib.pyplot import subplot, title, plot, colorbar
 from numpy             import amax, amin
+from matplotlib        import use
+use("Agg")
 
-import matplotlib.animation as animation
-import matplotlib.pyplot    as plt
 
-ftsz_title = 15
-ftsz_label = 12
+ftsz_title_big = 27
+ftsz_title     = 23
+ftsz_label     = 20
+models_name    = {'IC': 'Interchange', 'HW_mod': 'Modified Hasegawa Wakatani', 'HW_ord': 'Ordinary Hasegawa Wakatani', "IC_HW_mod": 'Interchange + Modified HW', 
+                  "IC_HW_ord": 'Interchange + Ordinary HW', 'HW_mod_IC': 'Modified HW + Interchange', 'HW_ord_IC':'Ordinary HW + Interchange'}
+
 cbar       = []
-
-min_time,         max_time         = 0, 0
-min_int_vort,     max_int_vort     = 0, 0
-min_Mass,         max_Mass         = 0, 0
-min_int_vort_sqr, max_int_vort_sqr = 0, 0
 
 def Analyzed (File_name):
     '''
@@ -27,19 +21,11 @@ def Analyzed (File_name):
     CM, Potential, density and vorticity-
     '''
     
-    global min_time, min_int_vort, min_Mass, min_int_vort_sqr
-    global max_time, max_int_vort, max_Mass, max_int_vort_sqr
-    
     Analytics = Analyse(File_name)
     
     Analytics.int_vort     = Analytics.integrate('vorticity')
     Analytics.int_vort_sqr = Analytics.integrate(Analytics.vorticity ** 2)
-    
-    min_time         = amin(Analytics.time);         max_time         = amax(Analytics.time)
-    min_int_vort     = amin(Analytics.int_vort);     max_int_vort     = amax(Analytics.int_vort)
-    min_Mass         = amin(Analytics.Mass);         max_Mass         = amax(Analytics.Mass)
-    min_int_vort_sqr = amin(Analytics.int_vort_sqr); max_int_vort_sqr = amax(Analytics.int_vort_sqr)
-    
+
     return Analytics
 
 def animate (position, Analytics, ax, fig):
@@ -51,26 +37,29 @@ def animate (position, Analytics, ax, fig):
         for j in range(2):
             ax[j, i].clear()
     ax = init(Analytics, ax, fig, None, pst = position, suptitle = False)
+    time_pos = Analytics.time[position]
 
-    ### Plots
-    ## Center of mass    
-    ## Integrated Vorticity
-    ax[0, 0].plot(Analytics.time[:position+1], Analytics.int_vort[:position+1], color='tab:blue')
-    
+    # Plots
+    ## Radial Velocity
+    ax[0, 0].plot(time_pos, Analytics.V_r[position], 'o', color='tab:red')
+    ax[0, 0].annotate(round(time_pos, 1), (time_pos, Analytics.V_r[position]), fontsize = ftsz_label)
+
     ## Mass
-    ax[0, 1].plot(Analytics.time[:position+1], Analytics.Mass[:position+1], color='tab:blue')
+    ax[0, 1].plot(time_pos, Analytics.Mass[position], 'o',  color='tab:red')
+    ax[0, 1].annotate(round(time_pos, 1), (time_pos, Analytics.Mass[position]), fontsize = ftsz_label)
 
-    
     ## Integrated Vorticity square ** 2
-    ax[0, 2].plot(Analytics.time[:position+1], Analytics.int_vort_sqr[:position+1], color='tab:blue')
+    ax[0, 2].plot(time_pos, Analytics.int_vort_sqr[position], 'o', color='tab:red')
+    ax[0, 2].annotate(round(time_pos, 1), (time_pos, Analytics.int_vort_sqr[position]), fontsize = ftsz_label)
 
-    ## The colormesh get updated when colling init
-    ## This happens because we wanna put the colorbar from the biegining
+
+    ## The colormesh get updated when calling init
+    ## This happens because we wanna put the colorbar from the begining
  
     return ax
 
 
-def init(Analytics, ax, fig, model, pst = 0, suptitle = True):
+def init(Analytics, ax, fig, model, extra = '', pst = 0, suptitle = True):
     '''
     Initialization of the plots, set the limits in the graphics, labels, titles and scales
     '''
@@ -78,42 +67,38 @@ def init(Analytics, ax, fig, model, pst = 0, suptitle = True):
     global cbar
     
     if suptitle:
-        fig.suptitle(model, fontsize=20)
-    
-    ## Integrated Vorticity
-    ax[0, 0].set_xlabel('Time', fontsize = ftsz_label)
-    ax[0, 0].set_ylabel(r'Integrated $\omega$', fontsize = ftsz_label)
-    ax[0, 0].set_xlim(min_time - 0.5, max_time + 0.5)
-    ax[0, 0].set_ylim(min_int_vort - 0.5, max_int_vort + 0.5)
-    ax[0, 0].set_title('Integrated Vorticity', fontsize = ftsz_title)
+        fig.suptitle(models_name[model] + '\n' + r'lx = {} ly = {} $\nu$ = {} dt = {:1.4f}'.
+                     format(Analytics.lx, Analytics.ly,
+                     Analytics.input['nu_perp'], Analytics.dt),
+                     fontsize = ftsz_title_big)
+                     
+    ## Average Radial Velocity
+    ax[0, 0].set_xlabel(r't [$\omega_{ci}^{-1}$]', fontsize = ftsz_label)
+    ax[0, 0].set_ylabel(r'Average $V_r$ [$\omega_{ci}\rho_s$]', fontsize = ftsz_label) 
+    ax[0, 0].plot(Analytics.time, Analytics.V_r, color='tab:blue')
+    ax[0, 0].set_title('Average Radial transport', fontsize = ftsz_title)
     
     ## Total Mass
-    ax[0, 1].set_xlabel('Time', fontsize = ftsz_label)
-    ax[0, 1].set_ylabel('Error in total Mass', fontsize = ftsz_label)
-    ax[0, 1].set_xlim(min_time - 0.5, max_time + 0.5)
-    ax[0, 1].set_ylim(min_Mass, max_Mass + 0.01)
-#     ax[0, 1].set_xscale('symlog')
-    ax[0, 1].set_title('Mass', fontsize = ftsz_title)
+    ax[0, 1].set_xlabel(r't [$\omega_{ci}^{-1}]$', fontsize = ftsz_label)
+    ax[0, 1].set_ylabel(r'n [$N$]', fontsize = ftsz_label)
+    ax[0, 1].plot(Analytics.time, Analytics.Mass, color='tab:blue')
+    ax[0, 1].set_title('Average density', fontsize = ftsz_title)
     
     ## Integrated Vorticity square ** 2
-    ax[0, 2].set_xlabel('Time', fontsize = ftsz_label)
-    ax[0, 2].set_ylabel(r'Integrated $\omega^2$', fontsize = ftsz_label)
-    ax[0, 2].set_xlim(min_time - 0.5, max_time + 0.5)
-    ax[0, 2].set_ylim(min_int_vort_sqr - 0.5, max_int_vort_sqr + 0.5)
-    ax[0, 2].set_title('Integrated Vorticity Square', fontsize = ftsz_title)
+    ax[0, 2].set_xlabel(r't [$\omega_{ci}^{-1}$]', fontsize = ftsz_label)
+    ax[0, 2].set_ylabel(r'$\omega^2$ [$\omega_{ci}^{2}$]', fontsize = ftsz_label)
+    ax[0, 2].plot(Analytics.time, Analytics.int_vort_sqr, color='tab:blue')
+    ax[0, 2].set_title('Average Vorticity Square', fontsize = ftsz_title)
     
     ## Pcolormesh
     ## Potential, density, Vorticity
-    ax[1, 0].set_title('Potential', fontsize = ftsz_title)
-    ax[1, 1].set_title('Ions density', fontsize = ftsz_title)
-    ax[1, 2].set_title('Vorticity', fontsize = ftsz_title)
+    ax[1, 0].set_title(r'Potential [$e / T_e$]', fontsize = ftsz_title)
+    ax[1, 1].set_title(r'Ions density [$N$]', fontsize = ftsz_title)
+    ax[1, 2].set_title(r'Vorticity [$\omega_{ci}$]', fontsize = ftsz_title)
     
-    im1 = ax[1, 0].pcolormesh(Analytics.x, Analytics.y, Analytics.potential[pst], cmap = 'hot', shading = 'gouraud');
-
-    
-    im2 = ax[1, 1].pcolormesh(Analytics.x, Analytics.y, Analytics.ions[pst], cmap = 'hot', shading = 'gouraud');
-    
-    im3 = ax[1, 2].pcolormesh(Analytics.x, Analytics.y, Analytics.vorticity[pst], cmap = 'hot', shading = 'gouraud');
+    im1 = ax[1, 0].pcolormesh(Analytics.x, Analytics.y, Analytics.potential[pst], cmap = 'hot', shading = 'gouraud')
+    im2 = ax[1, 1].pcolormesh(Analytics.x, Analytics.y, Analytics.ions[pst], cmap = 'hot', shading = 'gouraud')
+    im3 = ax[1, 2].pcolormesh(Analytics.x, Analytics.y, Analytics.vorticity[pst], cmap = 'hot', shading = 'gouraud')
     
     if len(cbar) < 3:
         cbar.append(fig.colorbar(im1,ax=ax[1, 0]))
@@ -134,18 +119,22 @@ def init(Analytics, ax, fig, model, pst = 0, suptitle = True):
 # For contour with color mesh
 #=====================================================================================
 
-def init_mesh_contour(Analytics, ax, fig = None, model = None, suptitle = True):
+def init_mesh_contour(Analytics, ax, fig, model = None, extra = '',  suptitle = True):
     '''
     Initialization of the plots, set the limits in the graphics, labels, titles and scales
     '''
     
     if suptitle:
-        fig.suptitle(model, fontsize=20)
+        fig.suptitle(models_name[model] + '\n' + r'lx = {} ly = {} $\nu$ = {} dt = {:1.4f}'.
+                     format(Analytics.lx, Analytics.ly,
+                     Analytics.input['nu_perp'], Analytics.dt),
+                     fontsize = ftsz_title_big)
     
     ## Pcolormesh
-    ## Potential, density, Vorticity
-    ax[0].set_title('Potential over Ions density', fontsize = ftsz_title)
-    ax[1].set_title('Vorticity', fontsize = ftsz_title)
+    ## Radial Velocity, Potential, density, Vorticity
+    ax[0].set_title('Radial Velocity', fontsize = ftsz_title)
+    ax[1].set_title('Potential over Ions density', fontsize = ftsz_title)
+    ax[2].set_title('Vorticity', fontsize = ftsz_title)
     
     return ax
 
@@ -155,29 +144,37 @@ def animate_mesh_contour (position, Analytics, ax, fig):
     '''
     
     global cbar
-    
-    ## Pcolormesh
-    ## Potentia
-    
+        
     ax[0].clear()
     ax[1].clear()
-    ax = init_mesh_contour(Analytics, ax, suptitle = False)
+    ax[2].clear()
 
-    ax[0].contour(Analytics.x, Analytics.y, Analytics.potential[position], cmap = 'hot')
+    ## Contour
+    # Potential
+    ax = init_mesh_contour(Analytics, ax, suptitle = False)
+    ax[1].contour(Analytics.x, Analytics.y, Analytics.potential[position], cmap = 'hot')
+    
+    ## Pcolormesh
+    # Radial Velocity
+    im0 = ax[0].pcolormesh(Analytics.x, Analytics.y, Analytics.v_r[position], cmap = 'hot', shading = 'gouraud')
     
     ## Density
-    im1 = ax[0].pcolormesh(Analytics.x, Analytics.y, Analytics.ions[position], cmap = 'hot', shading = 'gouraud')
+    im1 = ax[1].pcolormesh(Analytics.x, Analytics.y, Analytics.ions[position], cmap = 'hot', shading = 'gouraud')
 
     ## Vorticity
-    im2 = ax[1].pcolormesh(Analytics.x, Analytics.y, Analytics.vorticity[position], cmap = 'hot', shading = 'gouraud')
+    im2 = ax[2].pcolormesh(Analytics.x, Analytics.y, Analytics.vorticity[position], cmap = 'hot', shading = 'gouraud')
     if len(cbar) < 2:
-        cbar.append(fig.colorbar(im1,ax=ax[0]))
-        cbar.append(fig.colorbar(im2,ax=ax[1]))
+        cbar.append(fig.colorbar(im0, ax = ax[0]))
+        cbar.append(fig.colorbar(im1, ax = ax[1]))
+        cbar.append(fig.colorbar(im2, ax = ax[2]))
     else:
-        cbar[0].mappable.set_clim(vmin = amin(Analytics.ions[position]),
+        cbar[0].mappable.set_clim(vmin = amin(Analytics.v_r[position]),
+                                  vmax = amax(Analytics.v_r[position]))
+        
+        cbar[1].mappable.set_clim(vmin = amin(Analytics.ions[position]),
                                   vmax = amax(Analytics.ions[position]))
                                   
-        cbar[1].mappable.set_clim(vmin = amin(Analytics.vorticity[position]),
+        cbar[2].mappable.set_clim(vmin = amin(Analytics.vorticity[position]),
                                   vmax = amax(Analytics.vorticity[position]))
                                   
     return ax
