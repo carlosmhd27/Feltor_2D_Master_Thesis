43a7a011a8e6488c71bd0ab2d16740e874cb7e91
Carlos Rodríguez <carlos_mhd@pop-os.localdomain>
2021-03-28 19:49:15 +0200

    x_a and x_b has are taken as percentage over the lx grid, they must be given as float values from 0 to 1 (same as posX and posY)

--- FELTOR-Complete/parameters.h
+++ FELTOR-Complete/parameters.h
@@ -13,7 +13,6 @@ struct Parameters
 {
 	std::string model;
 	bool modified;
-	double tanh_width, x_a, x_b;
 	unsigned n, Nx, Ny;
 	double dt;
 	unsigned n_out, Nx_out, Ny_out;
@@ -28,17 +27,15 @@ struct Parameters
 	double amp, sigma, posX, posY;
 
 	double lx, ly;
+	double tanh_width, x_a, x_b;
 	dg::bc bc_x_n, bc_y_n;
 	dg::bc bc_x_omega, bc_y_omega;
 	dg::bc bc_x_phi, bc_y_phi;
 
 
 	Parameters( const Json::Value& js) {
-	model    = js["model"].asString();
-	modified = js["modified"].asBool();
-	tanh_width = js["tanh_width"].asDouble();
-	x_a        = js["x_a"].asDouble();
-	x_b        = js["x_b"].asDouble();
+	model      = js["model"].asString();
+	modified   = js["modified"].asBool();
 
 	n       = js["n"].asUInt();
 	Nx      = js["Nx"].asUInt();
@@ -50,23 +47,29 @@ struct Parameters
 	itstp   = js["itstp"].asUInt();
 	maxout  = js["maxout"].asUInt();
 
-	eps_pol     = js["eps_pol"].asDouble();
-	eps_time    = js["eps_time"].asDouble();
-	stages      = js.get("stages",3).asUInt();
-	kappa       = js["curvature"].asDouble();
-	g           = js["dens_prof"].asDouble();
-	alpha       = js["adiabatic"].asDouble();
-	sgm         = js["sheath_diss"].asDouble();
-	lambda      = js["sheath_pot"].asDouble();
-	tau         = js["tau"].asDouble();
-	nb          = js["nb"].asDouble();
-	nu          = js["nu_perp"].asDouble();     // Dissipation
-	amp         = js["amplitude"].asDouble();
-	sigma       = js["sigma"].asDouble();
-	posX        = js["posX"].asDouble();
-	posY        = js["posY"].asDouble();
-	lx          = js["lx"].asDouble();
-	ly          = js["ly"].asDouble();
+	eps_pol  = js["eps_pol"].asDouble();
+	eps_time = js["eps_time"].asDouble();
+	stages   = js.get("stages",3).asUInt();
+	kappa    = js["curvature"].asDouble();
+	g        = js["dens_prof"].asDouble();
+	alpha    = js["adiabatic"].asDouble();
+	sgm      = js["sheath_diss"].asDouble();
+	lambda   = js["sheath_pot"].asDouble();
+	tau      = js["tau"].asDouble();
+	nb       = js["nb"].asDouble();
+	nu       = js["nu_perp"].asDouble();     // Dissipation
+
+	amp   = js["amplitude"].asDouble();
+	sigma = js["sigma"].asDouble();
+
+	posX       = js["posX"].asDouble();
+	posY       = js["posY"].asDouble();
+	lx         = js["lx"].asDouble();
+	ly         = js["ly"].asDouble();
+	tanh_width = js["tanh_width"].asDouble();
+	x_a        = js["x_a"].asDouble() * lx;
+	x_b        = js["x_b"].asDouble() * ly;
+
 	bc_x_n      = dg::str2bc(js["bc_x_n"].asString());
 	bc_y_n      = dg::str2bc(js["bc_y_n"].asString());
 	bc_x_omega  = dg::str2bc(js["bc_x_omega"].asString());
@@ -80,25 +83,25 @@ struct Parameters
 			<< "Use the modified HW model: " << modified<<"\n";
 
 		os << "Position of the boundaries between models \n"
-			<< " x_a:  = " << x_a << "\n"
-			<< " x_b:  = " << x_b << "\n"
-			<< "width: = " << tanh_width << "\n";
+			<< "         x_a:          = " << x_a << "\n"
+			<< "         x_b:          = " << x_b << "\n"
+			<< "width of the boundary: = " << tanh_width << "\n";
 
         os << "Physical parameters are: \n"
-            <<"    Viscosity:       = "<<nu<<"\n"
-            <<"    Curvature:       = "<<kappa<<"\n"
-			<<"  density profile:   = "<<g<<"\n"
-			<<" adiabatic parameter = "<<alpha<<"\n"
-			<<" Sheath dissipation: = "<<sgm<<"\n"
-			<<"  Sheath potential:  = "<<lambda<<"\n"
-			<<"   Source's tau      = "<<tau<<"\n"
-			<<"       nb            = "<<nb<<"\n";
+            <<"              Viscosity:              = "<<nu<<"\n"
+            <<"              Curvature:              = "<<kappa<<"\n"
+			<<"Density profile (not in use anymore): = "<<g<<"\n"
+			<<"           adiabatic parameter        = "<<alpha<<"\n"
+			<<"           Sheath dissipation:        = "<<sgm<<"\n"
+			<<"            Sheath potential:         = "<<lambda<<"\n"
+			<<"             Source's tau             = "<<tau<<"\n"
+			<<"                 nb                   = "<<nb<<"\n";
 
         os  <<"Blob parameters are: \n"
-            << "    width:        "<<sigma<<"\n"
-            << "    amplitude:    "<<amp<<"\n"
-            << "    posX:         "<<posX<<"\n"
-            << "    posY:         "<<posY<<"\n";
+            << "     width:     = "<<sigma<<"\n"
+            << "   amplitude:   = "<<amp<<"\n"
+            << "     posX:      =  "<<posX<<"\n"
+            << "     posY:      =  "<<posY<<"\n";
 
         os << "Boundary parameters are: \n"
             <<"    lx = "<<lx<<"\n"
